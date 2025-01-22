3e49c0624c6d6de6ece16fa38c699579b1edebe8
Juanma Molins Cortés <35576283+jmmolins87@users.noreply.github.com>
2025-01-22 12:51:31 +0100

    add routes and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./public/public.module').then( m => m.PublicModule ) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule ) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
