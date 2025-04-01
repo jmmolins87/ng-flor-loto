cfcd06ee98151ef40204036056cd0636a80d8433
Juanma Molins Cortés <35576283+jmmolins87@users.noreply.github.com>
2025-04-01 08:22:06 +0200

    add jarron aromas product

--- src/app/public/components/gallery-image/gallery-image.component.ts
+++ src/app/public/components/gallery-image/gallery-image.component.ts
@@ -18,8 +18,11 @@ export class GalleryImageComponent implements OnInit, OnDestroy {
   @Input()
   public altImg!: string;
 
-  public currentImageIndex: number = 0;
   private autoSlideInterval!: any;
+
+  public currentImageIndex: number = 0;
+  public isPreviewOpen: boolean = false;
+  public currentPreviewImage: string = '';
 
   ngOnInit(): void {
     this.startAutoSlide();
@@ -27,6 +30,7 @@ export class GalleryImageComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.stopAutoSlide();
+    document.body.style.overflow = '';
   }
 
   nextImage(): void {
@@ -49,6 +53,36 @@ export class GalleryImageComponent implements OnInit, OnDestroy {
     return `translateX(-${this.currentImageIndex * 100}%)`;
   }
 
+  openPreview(image: string): void {
+    this.isPreviewOpen = true;
+    this.currentPreviewImage = image;
+  }
+
+  closePreview(): void {
+    this.isPreviewOpen = false;
+  }
+
+  onImageError(event: Event): void {
+    const target = event.target as HTMLImageElement;
+    target.src = '/assets/img/logo-new-loto.jpg';
+  }
+
+  prevPreviewImage(): void {
+    const currentIndex = this.images.indexOf(this.currentPreviewImage);
+    const prevIndex = (currentIndex - 1 + this.images.length) % this.images.length;
+    this.currentPreviewImage = this.images[prevIndex];
+  }
+
+  nextPreviewImage(): void {
+    const currentIndex = this.images.indexOf(this.currentPreviewImage);
+    const nextIndex = (currentIndex + 1) % this.images.length;
+    this.currentPreviewImage = this.images[nextIndex];
+  }
+
+  selectPreviewImage(image: string): void {
+    this.currentPreviewImage = image;
+  }
+
   private startAutoSlide(): void {
     this.autoSlideInterval = setInterval(() => {
       this.nextImage();
@@ -64,8 +98,4 @@ export class GalleryImageComponent implements OnInit, OnDestroy {
     this.startAutoSlide();
   }
 
-  onImageError(event: Event): void {
-    const target = event.target as HTMLImageElement;
-    target.src = '/assets/img/logo-new-loto.jpg';
-  }
 }
